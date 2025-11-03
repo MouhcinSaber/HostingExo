fb88397738a8f5b0ccc2d85c505cd9c41d02003c
Mouhcin Saber <m.benyammi.16@gmail.com>
2025-11-03 17:11:27 +0100

    commintig

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ app.use(express.static('dist', {
     etag: false
 }));
 
-app.get('/.*/', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 });
